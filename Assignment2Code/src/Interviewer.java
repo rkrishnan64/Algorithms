9af4a4a1e05099b88931c124b7331ff250646252
Gordon Liang <gordon@heymabes.com>
2014-06-14 13:30:57 -0700

    Adding Interviewer Files

---
+++ Assignment2Code/src/Interviewer.java
@@ -0,0 +1,20 @@
+
+public class Interviewer 
+{
+	String Name;
+	
+	Interviewer()
+	{
+		Name = null;
+	}
+	
+	public String getName()
+	{
+		return Name;
+	}
+	
+	public void setName(String NewName)
+	{
+		Name = NewName;
+	}
+}
