7e8ac5e8f6690d9f35ac76a4fae85eadfd955e44
Rohit Krishnan <rohitkrishnan@yahoo.com>
2014-06-15 00:29:38 -0700

    inner class

--- Assignment2Code/src/HumanResources.java
+++ Assignment2Code/src/HumanResources.java
@@ -1,4 +1,36 @@
 
 public class HumanResources {
+	public String name;
+	public String department;
+	public String description;
+	public int ID;
+	class Job {
+		String name;
+		String department;
+		String description;
+		int ID;
+		
+		
+		
+		public Job(String jobName, String jobDepartment, 
+				String jobDescription, int jobID){
+			    name = jobName;
+			    department = jobDepartment;
+			    description = jobDescription;
+			    ID = jobID;
+			  }
+		
+
+	}
+	
+	public HumanResources(){
+	
+		
+	}
+	
+	public void createJobManager(String jobName, String jobDepartment, 
+			String jobDespcription, int jobID){
+		
+	}
 
 }
