cdb02e1f44b03e53cefb6d5efa5e633b69ed9295
Rohit Krishnan <rohitkrishnan@yahoo.com>
2014-06-17 19:49:02 -0700

    added basic Applicant rating functionality

--- Assignment2Code/src/ApplicantRating.java
+++ Assignment2Code/src/ApplicantRating.java
@@ -1,4 +1,23 @@
 
 public class ApplicantRating {
+	public float exp;
+	public float att;
+	public float sal;
+	public float rating;
+	
+	
+	public void setRating(float experience, float attitude, float salary){
+			exp = experience;
+			att = attitude;
+			sal = salary;
+		    rating = exp + att + sal/3;
+		
+		}
+	public float getRating(){
+		return rating;
+		}
+	
+	
+	
 
 }
