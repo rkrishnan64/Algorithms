9a4f1f43f43429cc551773105d729b519a1c60a7
apoorvashastry <apoorva.shastry@yahoo.co.in>
2014-06-19 14:54:30 -0700

    Changed ArrayList to Hashmap

--- Assignment2Code/src/JobManager.java
+++ Assignment2Code/src/JobManager.java
@@ -1,70 +1,61 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class JobManager 
 
 {
 	private int nextJobID;
-	private ArrayList<Job> newJobList = new ArrayList<Job>();
-	
+	private HashMap<Integer,Job> JobList = new HashMap<Integer,Job>();
+	Job j=new Job();
 	public void CreateJob(String jobName,String jobDescription,String jobDepartment,int nextjobId)
 	{
-		Job j=new Job();
+		
 		j.setJobName(jobName);
 		j.setJobDescription(jobDescription);
 		j.setJobDepartment(jobDepartment);
+		j.setJobID(nextjobId);
 	    this.nextJobID=nextjobId;
-	    newJobList.add(j);
+	    JobList.put(j.getJobID(),j);
 	    
 	}
 	
 	public void deleteJob(int jobId)
 
-	{
-		boolean jobDeleted=false;
-		for(int i=0;i<newJobList.size();i++)
-		{
-			if(newJobList.get(i).getJobID()==jobId)
-			{
-				newJobList.remove(i);
-				jobDeleted=true;
-			}
-			
-		}
-		if(jobDeleted==false)
-		{
-			System.out.println("Incorrect JobID, Cannot delete the Job");
-		}
-		else
-		{
-			System.out.println("Job Deleted successfully");
-		}
+	{    
+		   
+			j=JobList.get(jobId);
+			if(j!=null)
+			    {
+					JobList.remove(jobId);
+					System.out.println("Job deleted successfully");
+				}
+				else
+				{
+					System.out.println("incorrect jobID cnnot delete job");
+		    	}
 		
 	}
 	
 	public void markJobStatus(int jobId,boolean jobFilled)
 	{
-		//Job j=new Job();
-		boolean jobStatus;
-		boolean set=false;
-		for (int i=0;i<newJobList.size();i++)
-		{
-			if(newJobList.get(i).getJobID()==jobId)
-			{
-			 jobStatus=newJobList.get(i).isJobFilled();
-			 jobStatus=jobFilled;
-			 set=true;
-			}
-		}
-		if(set==true)
-		{
-			System.out.println("Job Status changed successfully");
+
 		
+		 
+		j=JobList.get(jobId);
+		if(j!=null)
+		{
+			j.setJobFilled(jobFilled);
+			System.out.println("Job Status changed successfully");
 		}
+			
+		
 		else
 		{
 			System.out.println("Incorrect JobId, Cannot change the status");
 		}
 	}
+	
+
 	
 }
